c35c7644fc5eb6a7cb3a90c7e678aea758780d6a
ydabrilp <ydabrilp@ufpso.edu.co>
2024-09-03 14:43:19 -0500

    architecture definition

---
+++ src/main/java/com/emazon/msuser/MsuserApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.msuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/msuser/MsuserApplicationTests.java
@@ -0,0 +1,8 @@
+package com.emazon.msuser;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsuserApplicationTests {
+
+}
